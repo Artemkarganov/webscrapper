5ff638a081ae3824388a35c9a40c68e111451146
Artemkarganov <artem.karganov@gmail.com>
2018-01-26 08:29:02 +0200

    fixed scapping of one page (now scapp all the pages), added titles for csv file rows, added rescue for NoMethodError, created a class with method for scrapping in it

--- web_scrapper_test.rb
+++ web_scrapper_test.rb
@@ -3,37 +3,76 @@ require 'nokogiri'
 require 'json'
 require 'pry'
 require 'csv'
+class Scrapper
 
-page = HTTParty.get('https://www.olx.ua/zhivotnye/sobaki/chk/')
+  def scrap(url)
 
-parse_page = Nokogiri::HTML(page)
+    page = HTTParty.get(url)
 
-entries = parse_page.css('.wrap').css('.offer')
+    entriesArray = []
 
-entriesArray = []
+    #set the fir row of array the column names
+    entriesArray << ['Title', 'Price', 'Time', 'Image', 'href']
 
-entries.each do |entry|
+    next_page = false
 
-  image = entry.css('img.fleft')[0]['src']
-  title = entry.css('.rel>h3>a>strong').text
-  href = entry.css('.rel>h3>a')[0]['href']
-  price = entry.css('p.price>strong')[0].text
-  time = entry.css('p.x-normal')[0].text
+    page_num = 1
 
+    puts 'scrapping started'
 
-  entriesArray << image
-  entriesArray << title
-  entriesArray << href
-  entriesArray << price
-  entriesArray << time
+    while next_page == false
+
+      parse_page = Nokogiri::HTML(page)
+
+      entries = parse_page.css('.wrap').css('.offer')
+
+      entries.each do |entry|
+        begin
+          image = entry.css('img.fleft')[0]['src']
+        rescue NoMethodError
+          puts 'Image not found'
+        else
+          begin
+           title = entry.css('.rel>h3>a>strong')[0].text
+          rescue NoMethodError
+            puts "Title not found"
+          else
+            href = entry.css('.rel>h3>a')[0]['href']
+            begin
+             price = entry.css('p.price>strong')[0].text
+            rescue NoMethodError
+             puts "Price not found"
+            else
+            time = entry.css('p.x-normal')[0].text.strip
+
+            entriesArray << [title, price, time, image, href]
+            end
+          end
+        end
+      end
+
+        CSV.open('ad.csv', 'w+') do |csv|
+          entriesArray.each do |row|
+          csv << row
+          end
+        end
+
+        nextLink = entries.xpath("//span[@class='fbold next abs large']/a").map { |link| link['href'] }
+        if nextLink.any?
+         page = HTTParty.get(url + '?page='+"#{page_num+1}")
+         a = url + '?page='+"#{page_num + 1}"
+         puts nextLink
+          puts a
+          puts "next page go go"
+        else
+         next_page = true
+         puts "stop"
+        end
+        page_num += 1
+      end
+  end
 end
-
-entries
-
-
-CSV.open('ad.csv', 'w') do |csv|
-  csv << entriesArray
-end
-
+scrapper = Scrapper.new
+scrapper.scrap('https://www.olx.ua/zhivotnye/chk/')
 #used for debugging
 Pry.start(binding)
